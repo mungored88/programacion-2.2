889474df837e4ecba7abdecbf5190351f1da153e
gonzalo.morenop <gonzalo.morenop@davinci.edu.ar>
2020-11-23 19:32:53 -0300

    SaveData

---
+++ programacion 2.2/Assets/Scripts/DataToSave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class DataToSave 
+{
+    public int level;
+    public Checkpoint checkPoint;
+    public int lifes;
+    public Data2 data;
+}
+
+[Serializable]
+public class Data2
+{
+    public int a;
+    public float b;
+}
+
+public class Checkpoint
+{
+    public float X;
+    public float Y;
+    public float Z;
+}
+
+
+
+//var time = DateTime.Now;
+//print(time);
+//var json = JsonUtility.ToJson((JsonDateTime)time);
+//print(json);
+//DateTime timeFromJson = JsonUtility.FromJson<JsonDateTime>(json);
+//print(timeFromJson);
---
+++ programacion 2.2/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+[Serializable]
+public class SaveLoad : MonoBehaviour
+{
+   
+    public string _path;
+    private string _SAVEPATH = "/Save.json";
+    void Start()
+    {
+        _path = Application.streamingAssetsPath + _SAVEPATH;
+        //_path = Application.persistentDataPath + _SAVEPATH;
+        //Debug.Log(_path);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SaveFile();
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            DataToSave data = LoadFile();
+            Debug.Log(data.level);
+        }
+
+    }
+
+    public DataToSave LoadFile()
+    {
+        if (!File.Exists(_path))
+        {
+            SaveFile();
+        }
+
+        string data = File.ReadAllText(_path);
+        return JsonUtility.FromJson<DataToSave>(data);
+    }
+
+    public void SaveFile()
+    {
+        var data = new DataToSave();
+        data.level = 1;
+
+
+        StreamWriter file = File.CreateText(_path);
+        string json = JsonUtility.ToJson(data, true);
+
+        file.Write(json);
+        file.Close();
+    }
+}
