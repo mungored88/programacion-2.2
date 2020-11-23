409825184d5a85f8d61230521d45ed7799f9fe6a
mariano <marianoa.munguia@gmail.com>
2020-11-23 19:37:11 -0300

    ads manager

---
+++ programacion 2.2/Assets/Scripts/AddsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AddsManager : MonoBehaviour
+{
+    public enum TypeAds
+    {
+        video,
+        rewardedVideo
+    };
+    public TypeAds currentType;
+
+    string _gameId = "3913047";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(_gameId, false);
+    }
+
+  
+    public void Show() 
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show(currentType.ToString(), new ShowOptions() {resultCallback = ResulAds});
+        }
+    }
+    void ResulAds(ShowResult result)
+    {
+        if (result==ShowResult.Failed)
+        {
+            //no da nada
+        }
+        else if(result==ShowResult.Skipped)
+        {
+
+        } else
+        {
+            //dar vida
+        }
+    }
+}
