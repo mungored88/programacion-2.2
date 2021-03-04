beb9f8682abe6cd8b03ea8f2aa8d6825ed602266
mariano <marianoa.munguia@gmail.com>
2021-03-04 19:23:21 -0300

    select level final

---
+++ programacion 2.2/Assets/hackactive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hackactive : MonoBehaviour
+{
+    public GameObject hack;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Alpha0)) hack.gameObject.SetActive(true);
+    }
+}
