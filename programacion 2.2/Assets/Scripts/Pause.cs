f3037585238106174dfed48aa34be7b760da43b8
mariano <marianoa.munguia@gmail.com>
2021-03-05 02:53:17 -0300

    MINI MENU

---
+++ programacion 2.2/Assets/Scripts/Pause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    
+        public GameObject menu;
+
+        public void PauseGame()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = 1;
+        }
+    }
---
+++ programacion 2.2/Assets/Scripts/SetVolumen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolumen : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string Fader;
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat(Fader, Mathf.Log10(sliderValue) * 20);
+    }
+}
